a6a8695cb3da0753895a55b5be553294f4ded542
kavithaseran <kavithaseran@gmail.com>
2018-10-17 15:30:17 +0530

    deletion

--- Mnop.java
+++
@@ -1,9 +0,0 @@
-public class Mnop
-{
-    public void display()
-    {
-        System.out.println("This is Mnop");
-        System.out.println("This is another message - mnop");
-        System.out.println("This is another new message -mnop");
-    }
-}
